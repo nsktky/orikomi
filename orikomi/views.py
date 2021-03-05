b86299d65397c3f19e789a1f326b3da84e92fbbe
nsktky <nosaka.takuya11@gmail.com>
2021-03-05 23:58:12 +0900

    signupの実装

--- orikomi/views.py
+++ orikomi/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+# サインイン関数。新規ユーザーの作成と重複登録を防ぐ処理をする
 def signupfunc(request):
-    return render(request, 'signup.html', {})
+    # レスポンスがpostだった場合（signup.htmlのform入力があった場合）の動作
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # ユーザー作成時、重複していたらexcept
+        try:
+            # create_userでユーザーを作成。引数にユーザー名、メールアドレス、パスワードを渡す
+            user = User.objects.create_user(username, '', password)
+            # renderメソッドはhttpレスポンスを作成する
+            return render(request, 'signup.html', {'some': 100})
+        except IntegrityError:
+            return render(request, 'signup.html', {'error': 'このユーザーは登録されています。'})
+
+    return render(request, 'signup.html')
