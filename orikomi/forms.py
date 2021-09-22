74d55381236b173515a68f8df409a386e2958192
nsktky <nosaka.takuya11@gmail.com>
2021-09-22 21:31:32 +0900

    問い合わせフォームのデザイン

--- orikomi/forms.py
+++ orikomi/forms.py
@@ -12,14 +12,14 @@ class InquiryForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields['name'].widget.attrs['class'] = 'form-control col-9'
-        self.fields['name'].widget.attrs['placeholder'] = 'お名前をここに入力してください。'
-        self.fields['email'].widget.attrs['class'] = 'form-control col-11'
-        self.fields['email'].widget.attrs['placeholder'] = 'メールアドレスをここに入力してください。'
-        self.fields['title'].widget.attrs['class'] = 'form-control col-11'
-        self.fields['title'].widget.attrs['placeholder'] = 'タイトルをここに入力してください。'
+        self.fields['name'].widget.attrs['class'] = 'form-control col-12'
+        self.fields['name'].widget.attrs['placeholder'] = 'お名前'
+        self.fields['email'].widget.attrs['class'] = 'form-control col-12'
+        self.fields['email'].widget.attrs['placeholder'] = 'メールアドレス'
+        self.fields['title'].widget.attrs['class'] = 'form-control col-12'
+        self.fields['title'].widget.attrs['placeholder'] = 'タイトル'
         self.fields['message'].widget.attrs['class'] = 'form-control col-12'
-        self.fields['message'].widget.attrs['placeholder'] = 'メッセージをここに入力してください。'
+        self.fields['message'].widget.attrs['placeholder'] = 'メッセージ内容'
 
     def send_email(self):
         name = self.cleaned_data['name']
